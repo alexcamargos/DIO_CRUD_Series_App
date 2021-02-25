9b163cd7a4a62b4452bb658dc14bfbe834ba92d1
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2021-02-25 06:40:45 -0300

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO_CRUD_Series_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
